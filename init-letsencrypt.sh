db94e9eebc1b57887ebc521b2d16c853e3ee9c21
woraphol-j <woraphol.j@linecorp.com>
2022-03-26 17:23:25 +0700

    feat: change server_name for api to api-pm.efficient.guru

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(pm.efficient.guru)
+domains=(pm.efficient.guru api-pm.efficient.guru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="woraphol.j@appwow.co" # Adding a valid address is strongly recommended
